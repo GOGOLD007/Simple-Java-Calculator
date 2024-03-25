44a035f6549040e1eaf1a305a8b15d7501d1eacb
GOGOLD007 <80231597+GOGOLD007@users.noreply.github.com>
2024-03-25 14:32:29 +0000

    Added user instructions

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -5,23 +5,30 @@ import java.util.Scanner;
 public class BasicCalculator{
 	public static void main (String[]args){
 		Scanner in = new Scanner(System.in);
+		System.out.println("Type your first number:");
 		int num1 = in.nextInt();
+		System.out.println("Choose one of the operations:+,-,*,/");
 		String str = in.next();
+		System.out.println("Type your second number:");
 		int num2 = in.nextInt();
 		switch (str) {
 			case "+":
+				System.out.println("Answer:");
 				System.out.println(num1+num2);
 				break;
 
 			case "-":
+			System.out.println("Answer:");
 				System.out.println(num1-num2);
 				break;
 
 			case "*":
+			System.out.println("Answer:");
 				System.out.println(num1*num2);
 				break;
 
 			case "/":
+			System.out.println("Answer:");
 				System.out.println(num1/num2);
 				break;
 		
